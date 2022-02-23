99afb85da25a72ff47b90e1badfec1058b416288
Dulgiduck <jsh@sngy.io>
2022-02-23 10:27:46 +0900

    Feature: 스프링 MVC - 시작하기

---
+++ src/main/java/topia/duck/mvc/servlet/web/springmvc/v1/SpringMemberSaveControllerV1.java
@@ -0,0 +1,34 @@
+package topia.duck.mvc.servlet.web.springmvc.v1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import topia.duck.mvc.servlet.domain.member.Member;
+import topia.duck.mvc.servlet.domain.member.MemberRepository;
+import topia.duck.mvc.servlet.web.frontcontroller.ModelView;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Map;
+
+@Controller
+public class SpringMemberSaveControllerV1 {
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @RequestMapping("/springmvc/v1/members/save")
+    public ModelAndView process(HttpServletRequest request, HttpServletResponse response) {
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+
+        // Model에 데이터 보관
+        ModelAndView mv = new ModelAndView("save-result");
+        mv.addObject("member", member);
+        return mv;
+    }
+}
